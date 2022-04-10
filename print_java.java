ec714405747ffc02f200ca3f32b87bc0f14373aa
Abdur-Rahman Fashola <abbadshaf05@gmail.com>
2022-04-10 02:49:40 +0100

    Changed program desc.

--- print_java.java
+++ print_java.java
@@ -1,6 +1,6 @@
 
 /**
-* This program prints the sum of two user-selected numbers.
+* This program prints the result of two string's concatenation or sum of two user-selected numbers!
 * 
 * @author Abdur-Rahman Fashola
 * @version 1.0
@@ -10,70 +10,70 @@
 import java.util.Scanner;
 
 public class print_java {
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
 
-        // Prompt the user to select a mode for the CLI
-        System.out.print("Select mode (\"Concat Strings\" or \"Add Numbers\"): ");
-        
-        // Store the choice in a variable
-        String modePrompt = input.nextLine();
-        // Run the program based on the user's choice
-        String mode = selectMode(modePrompt);
+		// Prompt the user to select a mode for the CLI
+		System.out.print("Select mode (\"Concat Strings\" or \"Add Numbers\"): ");
 
-        if (mode == "concat") {
-            // Prompt user for a string input
-            System.out.printf("Enter a string to concatenate: ");
-            // Register the input and store in a var
-            String str1 = input.nextLine();
+		// Store the choice in a variable
+		String modePrompt = input.nextLine();
+		// Run the program based on the user's choice
+		String mode = selectMode(modePrompt);
 
-            // Prompt user for a second string..
-            System.out.printf("Enter another string: ");
-            // Store the second value in another var
-            String str2 = input.nextLine();
+		if (mode == "concat") {
+			// Prompt user for a string input
+			System.out.printf("Enter a string to concatenate: ");
+			// Register the input and store in a var
+			String str1 = input.nextLine();
 
-            // Invoke the `concatStrings` method to add and print the sum
-            concatStrings(str1, str2);
-            // Close the input Scanner stream
-            input.close();
-        } else if (mode == "add") {
-            // Prompt user for a number input
-            System.out.printf("Enter a number: ");
-            // Register the input and store in a var
-            int num1 = input.nextInt();
+			// Prompt user for a second string..
+			System.out.printf("Enter another string: ");
+			// Store the second value in another var
+			String str2 = input.nextLine();
 
-            // Prompt user for a second number..
-            System.out.printf("Enter another number: ");
-            // Store the second value in another var
-            int num2 = input.nextInt();
+			// Invoke the `concatStrings` method to add and print the sum
+			concatStrings(str1, str2);
+			// Close the input Scanner stream
+			input.close();
+		} else if (mode == "add") {
+			// Prompt user for a number input
+			System.out.printf("Enter a number: ");
+			// Register the input and store in a var
+			int num1 = input.nextInt();
 
-            // Invoke the `calculateSum` method to add and print the sum
-            calculateSum(num1, num2);
-            // Close the input Scanner stream
-            input.close();
-        } else {
-            System.out.println("Invalid option, re-try the program");
-            input.close();
-        }
-    }
+			// Prompt user for a second number..
+			System.out.printf("Enter another number: ");
+			// Store the second value in another var
+			int num2 = input.nextInt();
 
-    private static String selectMode(String mode) {
-        if (mode.contains("Concat Strings")) {
-            return "concat";
-        } else if (mode.contains("Add Numbers")) {
-            return "add";
-        } else {
-            return "Invalid option";
-        }
-    }
+			// Invoke the `calculateSum` method to add and print the sum
+			calculateSum(num1, num2);
+			// Close the input Scanner stream
+			input.close();
+		} else {
+			System.out.println("Invalid option, re-try the program");
+			input.close();
+		}
+	}
 
-    private static void calculateSum(int num1, int num2) {
-        int sum = num1 + num2;
-        System.out.println(sum);
-    }
+	private static String selectMode(String mode) {
+		if (mode.contains("Concat Strings")) {
+			return "concat";
+		} else if (mode.contains("Add Numbers")) {
+			return "add";
+		} else {
+			return "Invalid option";
+		}
+	}
 
-    private static void concatStrings(String str1, String str2) {
-        String concat = str1 + " " + str2;
-        System.out.println(concat);
-    }
+	private static void calculateSum(int num1, int num2) {
+		int sum = num1 + num2;
+		System.out.println(sum);
+	}
+
+	private static void concatStrings(String str1, String str2) {
+		String concat = str1 + " " + str2;
+		System.out.println(concat);
+	}
 }
